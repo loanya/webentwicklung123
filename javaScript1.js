7cef6d2af8dd900e5c2227a1cd03dc21433fc022
laryol <laryol2@gmx.de>
2021-11-15 10:13:26 +0100

    1

---
+++ javaScript1.js
@@ -0,0 +1,21 @@
+function abfrage(){
+
+    const mail = "huehne@htw-berlin.de"
+    const passwort= "hunter2"
+    var pwInput = document.querySelector('#password')
+    var mailInput = document.querySelector('#E-mail')
+    if (mail == mailInput.value && passwort== pwInput.value){
+
+        window.location ="Startseite_nach_Login.html"
+
+    }
+    else{
+        window.alert((pwInput.value))
+        window.alert("E-Mail und Passwort stimmen nicht überein")
+    }
+    window.alert("123")
+}
+const input = document.querySelector('#password')
+input.addEventListener('keyup', () =>{
+    console.log(input.value.length)
+})
