dc432212fbe0b4a76da0a92ad35c83037d56c2d8
loanya <s0556260@htw-berlin.de>
2021-12-31 01:29:54 +0100

    map

--- src/components/VacationPlanner/VacationEditPage.tsx
+++ src/components/VacationPlanner/VacationEditPage.tsx
@@ -1,7 +1,32 @@
 import "./VacationEditPage.css";
+import {useEffect, useState} from "react";
+
+import Api from "../../services/Api";
+
+type Vacation = {
+    id: string;
+    vacation_list_id: string;
+    vacation_name: string;
+    country_name: string;
+    start_date: Date;
+    end_date: Date;
+};
 
 const VacationEditPage = () => {
-    return <div className={"page-container"}> Vacation Edit Page </div>;
+    const [vacations, setVacations] = useState<Vacation[]>([])
+    useEffect(() => {
+        Api.getVacations()
+            .then(response => response.json())
+            .then(data => setVacations(data))
+    })
+    return (
+        <div className={"page-container"}> Vacation Edit Page
+            {vacations.map(vacation =>
+                <div>
+                    <div>{vacation.vacation_name}</div>
+                    <div>{vacation.country_name}</div>
+                </div>)}
+        </div>)
 };
 
 export default VacationEditPage;
